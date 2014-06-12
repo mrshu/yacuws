989f1ecbd3f206248072e6e3914b5b43bcf537ea
mr.Shu <mr@shu.io>
2014-06-12 23:26:36 +0200

    logging

--- yacuws.c
+++ yacuws.c
@@ -15,13 +15,48 @@
 #include <errno.h>
 
 #define PORT 12345
+#define BUFSIZE 8192
+
+#define log_error(x) log(x, 1)
+
+void log(char *str, int err) {
+        printf("%s\n", str);
+        if (err)
+                perror(strerror(errno));
+
+        exit(-1);
+}
 
 /* Reads the request from `request_fd`, finds the path,
  * checks its correctness and writes back requested file */
 void handle_request(int request_fd) {
         printf("Handling request!\n");
-        close(request_fd);
 
+        long ret;
+        char buffer[BUFSIZE + 1]
+
+        ret = read(request_fd, buffer, BUFSIZE);
+
+        if (ret == 0 || ret == -1) {
+                log_error("Error reading the request (read)");
+        }
+
+        /* Null terminate the buffer */
+        if (ret > 0 && ret <= BUFSIZE) {
+                buffer[ret] = '\0';
+        } else {
+                log_error("Error responding to the request (request too big)");
+        }
+
+        if (strncmp(buffer, "GET ", 4) != 0 || strncmp(buffer, "get ", 4) != 0) {
+                log_error("Error responding to the request (no GET)");
+        }
+
+        if (strstr(&buffer[5], "..")) {
+
+        }
+
+        close(request_fd);
 }
 
 int main(int argc, char** argv)
@@ -36,8 +71,7 @@ int main(int argc, char** argv)
         int pid;
 
         if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                printf("Error initalizing socket (socket)\n");
-                return -1;
+                log_error("Error initalizing socket (socket)");
         }
 
         server_addr.sin_family = AF_INET;
@@ -45,26 +79,21 @@ int main(int argc, char** argv)
         server_addr.sin_port = htons(PORT);
 
         if (bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-                printf("Error binding socket (bind)\n");
-                return -1;
+                log_error("Error binding socket (bind)");
         }
 
         if (listen(listen_fd, 5) < 0) {
-                printf("Error initalizing listening (listen)\n");
-                return -1;
+                log_error("Error initalizing listening (listen)");
         }
 
         while (1) {
                 len = sizeof(client_addr);
                 if ((request_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &len)) < 0) {
-                        printf("Error accepting request (accept)\n");
-                        perror(strerror(errno));
-                        return -1;
+                        log_error("Error accepting request (accept)");
                 }
 
                 if ((pid = fork()) < 0) {
-                        printf("Problem with forking!");
-                        return -1;
+                        log_error("Problem with forking!");
                 }
 
                 if (pid == 0) {
