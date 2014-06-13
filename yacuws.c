3c69d1464fdff94c7860783053133bd6a6ed0c65
mr.Shu <mr@shu.io>
2014-06-13 10:35:06 +0200

    Added directory listing

--- yacuws.c
+++ yacuws.c
@@ -10,10 +10,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <errno.h>
+#include <dirent.h>
 
 #include <magic.h>
 
@@ -53,19 +55,82 @@ void _log(char *str, int err)
         }
 }
 
+char* build_dir_listing(char* directory)
+{
+        char out[10* BUFSIZE];
+        char path[BUFSIZE];
+        DIR *dir;
+        struct dirent *ent;
+        struct stat statbuf;
+
+        int len = 0;
+
+        len += sprintf(out, "<html><head><title>Dir listing of %s</title></head><body><h1>Dir listing of %s</h1><ul>", directory, directory);
+        dprint_str(out);
+
+        if ((dir = opendir (directory)) != NULL) {
+                while ((ent = readdir (dir)) != NULL) {
+                        dprint_str(ent->d_name);
+
+                        snprintf(path, BUFSIZE, "%s/%s", directory, ent->d_name);
+                        stat(path, &statbuf);
+
+                        if (S_ISDIR(statbuf.st_mode))
+                                len += snprintf (&out[len], BUFSIZE, "<li><a href='/%s%s/'>%s</a></li>\n", directory, ent->d_name, ent->d_name);
+                        else
+                                len += snprintf (&out[len], BUFSIZE, "<li><a href='/%s%s'>%s</a></li>\n", directory, ent->d_name, ent->d_name);
+
+                        if (len > 10*BUFSIZE)
+                                break;
+                }
+                dprint_str(out);
+
+                snprintf (&out[len], BUFSIZE, "</ul></body></html>\n");
+                closedir (dir);
+        } else {
+                log_error("Error reading directory (opendir)");
+        }
+
+        return out;
+}
+
 /* Responds to a request with a given HTTP response code and file.
  * Note: also shutdowns and closes the socket file descriptor */
 int respond_with_file(char* response, char* filename, int target_fd)
 {
         char buffer[BUFSIZE];
+        char bigger_buffer[11*BUFSIZE];
         const char* mime;
+        const char* out;
 
         ssize_t len;
         long length;
+
         int file = open(filename, O_RDONLY);
 
         if (file == -1) {
                 log_debug("Error opening the requested file (open)");
+
+                dprint_str(filename);
+                dprint_str(&filename[strlen(filename) - 10]);
+                if (strlen(filename) > 10) {
+                        if (strncmp(&filename[strlen(filename) - 10], "index.html", 10) == 0) {
+                                filename[strlen(filename) - 10] = '\0';
+                                out = build_dir_listing(filename);
+
+                                dprint_str(out);
+
+                                snprintf(bigger_buffer, 11*BUFSIZE - 1, "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/html\r\nContent-Length: %ld\r\n\r\n%s", (long int) strlen(out), out);
+
+                                dprint_str(bigger_buffer);
+
+                                send(target_fd, bigger_buffer, strlen(bigger_buffer), MSG_NOSIGNAL);
+                                shutdown(target_fd, SHUT_RDWR);
+
+                                return 1;
+
+                        }
+                }
 
                 file = open("./htdocs/404.html", O_RDONLY);
                 if (file == -1) {
@@ -139,7 +204,7 @@ void handle_request(int request_fd)
 
         if (strlen(tmp) > 0) {
                 if (tmp[strlen(tmp) -1] == '/')
-                        snprintf(filename, BUFSIZE, ".%s/index.html", tmp);
+                        snprintf(filename, BUFSIZE, ".%sindex.html", tmp);
                 else
                         snprintf(filename, BUFSIZE, ".%s", tmp);
         } else {
